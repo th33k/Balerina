8dfa29e6b58d18fa413f37b41572c9902033fb7b
Anuradha Herath <anuradhaherath2001@gmail.com>
2024-10-13 16:18:14 +0530

    cards link added

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,28 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import ServiceCards from './Components/ServiseCards/ServiceCards';
-
- // Import Footer component
 import './App.css';
 import Home from './Components/Home/Home';
 import SignUp from './Components/Signup/Signup'; 
 import Login from './Components/Login/Login';
-// import AboutUs from './Components/AboutUs/AboutUs';
 import TaskerForm from './Components/TaskerForm/TaskerForm';
 import ProfilePage from './Components/Profilepage/Profilepage';
+
 const App = () => {
   return (
     <Router>
       <div className="app-container">
         <Navbar />  {/* The Navbar will remain common across all pages */}
 
-
         {/* Set up routing */}
         <Routes>
           <Route path="/" element={<Home />} />  {/* Home or landing page */}
           <Route path="/services" element={<ServiceCards />} />  {/* Services page */}
           <Route path="/about" element={<ProfilePage />} />  {/* About page */}
-          <Route path="/Signup" element={<SignUp/>} />  {/* Signup page */}
-          <Route path="/login" element={<Login/>} />  {/* Login page */}
+          <Route path="/signup" element={<SignUp />} />  {/* Signup page */}
+          <Route path="/login" element={<Login />} />  {/* Login page */}
           <Route path="/become-tasker" element={<TaskerForm />} />  {/* TaskerForm page */}
+          <Route path="/profilepage" element={<ProfilePage />} />  {/* Profile page */}
           {/* Add more routes as needed */}
         </Routes>
 
